630890ccdfcd7adedf0eddb9bb6719cc5ef43754
karanidenis <d.waweru@alustudent.com>
2023-10-01 12:14:57 +0300

    Pages added

--- hello_app/lib/main.dart
+++ hello_app/lib/main.dart
@@ -1,23 +1,175 @@
 import 'package:flutter/material.dart';
 
+// function to trigger build when the app is run
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomeRoute(),
+      '/second': (context) => const SecondRoute(),
+      '/third': (context) => const ThirdRoute(),
+    },
+  )); //MaterialApp
+}
+
+
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({Key? key}) : super(key: key);
+
+  ButtonStyle _buttonStyle() {
+    return ElevatedButton.styleFrom(
+      textStyle: const TextStyle(
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+      ),
+      primary: Colors.blue, // You can use another color here
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello World'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Hello World',
+              style: TextStyle(
+                fontSize: 55.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
+            Row(
+              // Use Row instead of Column
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  style: _buttonStyle(), // Apply the common button style
+                  child: const Text('To Second'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/second');
+                  },
+                ),
+                const SizedBox(width: 16.0), // Add horizontal spacing
+                ElevatedButton(
+                  style: _buttonStyle(), // Apply the common button style
+                  child: const Text('To Third!'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/third');
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Second Page",
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ), // AppBar
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              // Use Row instead of Column
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  child: const Text(
+                    'Back!',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16.0), // Add horizontal spacing
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/third');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    textStyle: const TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  child: const Text('To Third!'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ), // Center
+    ); // Scaffold
+  }
+}
+
+
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Third Page",
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ), // AppBar
+      body: Center(
+        child: Row(
+          // Use Row instead of Column
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+              child: const Text(
+                'Back!',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ), // Center
+    ); // Scaffold
+  }
 }
