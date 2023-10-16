df569139a02eae5d3748ff4c6e408b9eb84ee7b5
Praise-Atadja <p.atadja@alustudent.com>
2023-10-17 02:22:51 +0300

    registration page ui

--- WeatherMan_app/lib/Login.dart
+++ WeatherMan_app/lib/Login.dart
@@ -145,36 +145,152 @@ class RegistrationPage extends StatelessWidget {
   const RegistrationPage({Key? key}) : super(key: key);
   static const primarybackground = Color(0xFF1E213A);
 
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primarybackground,
       appBar: AppBar(
         backgroundColor: primarybackground,
-        title: const Text('Registration Page'),
         actions: [
           IconButton(
             icon: const Icon(Icons.navigate_next),
-            tooltip: 'Go to the hompage page',
+             tooltip: 'Go to the hompage page',
             onPressed: () {
               Navigator.pushNamed(context, '/home');
             },
           ),
-          // Add additional action buttons here
         ],
       ),
       body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Create a new account',
-              style: TextStyle(fontSize: 24.0),
-            ),
-            // Add registration form or widgets here
-          ],
+        child: Padding(
+          padding: EdgeInsets.only(top: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Login',
+                style: TextStyle(fontSize: 24.0, color: Colors.white),
+              ),
+              SizedBox(height: 20), // Add space between text and the form
+              LoginForm(),
+              // Add your login form widget here
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: <Widget>[
+        const TextField(
+          decoration: InputDecoration(
+            labelText: 'Full Name',
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(
+                    5), // Adjust the top radius to control the length
+                bottom: Radius.circular(
+                    5), // Adjust the bottom radius to control the length
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+              ),
+              // Optional: Set border radius
+            ),
+          ),
+          style: TextStyle(color: Colors.white),
+        ),
+        // Email TextField
+        const TextField(
+          decoration: InputDecoration(
+            labelText: 'Email',
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(
+                    5), // Adjust the top radius to control the length
+                bottom: Radius.circular(
+                    5), // Adjust the bottom radius to control the length
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+              ),
+              // Optional: Set border radius
+            ),
+          ),
+          style: TextStyle(color: Colors.white),
+        ),
+        const SizedBox(
+            height: 20), // Add space between email and password fields
+        // Password TextField
+        const TextField(
+          decoration: InputDecoration(
+            labelText: 'Password',
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+              ),
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(
+                    5), // Adjust the top radius to control the length
+                bottom: Radius.circular(
+                    5), // Adjust the bottom radius to control the length
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+              ),
+              // Optional: Set border radius
+            ),
+          ),
+          style: TextStyle(color: Colors.white),
+        ),
+        const SizedBox(
+            height: 20), // Add space between email and password fields
+        // Password TextField
+
+        const SizedBox(
+            height: 20), // Add space between password field and the button
+        ElevatedButton(
+          onPressed: () {
+            // Implement your login logic here
+          },
+          child: const Text('SIGN UP'),
+        ),
+        const SizedBox(height: 50), // Add space here (adjust as needed)
+        const Text(
+          'ALREADY HAVE AN ACCOUNT?',
+          style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.white, // Set text color to white
+              fontWeight: FontWeight.bold), // Make text bold),
+        ),
+      ],
+    ));
   }
 }
