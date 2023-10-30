81fc75067a0b6a47d43670c829b98124a3cc1970
Praise-Atadja <p.atadja@alustudent.com>
2023-10-30 12:28:51 +0200

    create feedback page

--- WeatherMan_app/lib/database_pages.dart/database.dart
+++ WeatherMan_app/lib/database_pages.dart/database.dart
@@ -1,35 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DataGeoMethods {
-  Future addUserDetails(Map<String, dynamic> userInfoMap) async {
-    return await FirebaseFirestore.instance
-        .collection("location")
-        .doc()
-        .set(userInfoMap);
-  }
-
-  Future<QuerySnapshot> getthisUserInfo(String location) async {
-    return await FirebaseFirestore.instance
-        .collection("location")
-        .where("Location", isEqualTo: location)
-        .get();
-  }
-
-  Future UpdateUserData(String timezone, String id) async {
-    return await FirebaseFirestore.instance
-        .collection("location")
-        .doc(id)
-        .update({"Time Zone": timezone});
-  }
-
-  Future DeleteUserData(String id) async {
-    return await FirebaseFirestore.instance
-        .collection("location")
-        .doc(id)
-        .delete();
-  }
-}
-
 class DataFeedMethods {
   Future addFeedDetails(Map<String, dynamic> userInfoMap) async {
     return await FirebaseFirestore.instance
