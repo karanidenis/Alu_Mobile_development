a8b131365bb7960d8e5b77ef3bab4ca5adee8901
Praise-Atadja <p.atadja@alustudent.com>
2023-10-31 15:07:32 +0200

    Create home.dart

---
+++ WeatherMan_app/lib/pages/home.dart
@@ -0,0 +1,197 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatefulWidget {
+  const UserProfile({super.key});
+
+  @override
+  _UserProfileState createState() => _UserProfileState();
+}
+
+class _UserProfileState extends State<UserProfile> {
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+
+  final currentUserId = FirebaseAuth.instance.currentUser!.uid;
+  final _firestore = FirebaseFirestore.instance;
+  final userRef = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser!.uid);
+
+  @override
+  void initState() {
+    super.initState();
+    // Load user data when the screen is first loaded
+    loadUserData();
+  }
+
+  void loadUserData() async {
+    final userData = await userRef.get();
+    if (userData.exists) {
+      final data = userData.data() as Map<String, dynamic>;
+      firstNameController.text = data['firstName'] ?? '';
+      lastNameController.text = data['lastName'] ?? '';
+      locationController.text = data['location'] ?? '';
+    }
+  }
+
+  void uploadUserData() async {
+    String firstName = firstNameController.text;
+    String lastName = lastNameController.text;
+    String location = locationController.text;
+
+    Map<String, dynamic> userData = {
+      "firstName": firstName,
+      "lastName": lastName,
+      "location": lastName,
+    };
+
+    await userRef.set(userData, SetOptions(merge: true));
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('User data updated.'),
+    ));
+  }
+
+  void deleteUserData() async {
+    await userRef.delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('User data deleted.'),
+    ));
+
+    // Clear the text fields
+    firstNameController.text = '';
+    lastNameController.text = '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1E213A),
+      body: SingleChildScrollView(
+        // Wrap the Column with SingleChildScrollView
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 100),
+            const Text(
+              'User Profile',
+              style: TextStyle(fontSize: 24.0, color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              // First Name TextField
+              controller: firstNameController,
+              decoration: const InputDecoration(
+                labelText: 'First Name',
+                labelStyle: TextStyle(color: Colors.white),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 5.0,
+                  vertical: 5.0,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(5),
+                    bottom: Radius.circular(5),
+                  ),
+                  
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              // Last Name TextField
+              controller: lastNameController,
+              decoration: const InputDecoration(
+                labelText: 'Last Name',
+                labelStyle: TextStyle(color: Colors.white),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 5.0,
+                  vertical: 5.0,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(5),
+                    bottom: Radius.circular(5),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              // Location TextField
+              controller: locationController,
+              decoration: const InputDecoration(
+                labelText: 'Location',
+                labelStyle: TextStyle(color: Colors.white),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 5.0,
+                  vertical: 5.0,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(5),
+                    bottom: Radius.circular(5),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                uploadUserData();
+              },
+              child: const Text('Save'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                deleteUserData();
+              },
+              child: const Text('Delete'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/third');
+              },
+              child: const Text('Checkout The Weather'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
