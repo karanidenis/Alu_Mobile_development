41c4d3f22e9774d700955b35811a7eaf8ffcf11a
Praise-Atadja <p.atadja@alustudent.com>
2023-11-18 23:07:09 +0200

    Update firestore_service.dart

--- WeatherMan_app/lib/services/firestore_service.dart
+++ WeatherMan_app/lib/services/firestore_service.dart
@@ -4,15 +4,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 Future<DocumentSnapshot<Map<String, dynamic>>> getUserData() async {
   final user = FirebaseAuth.instance.currentUser;
   final userUid = user?.uid;
-
   if (userUid != null) {
     final userData = await FirebaseFirestore.instance
         .collection('users') // Replace with your collection name
         .doc(userUid)
         .get();
-
     return userData;
   }
-
   return Future.error('User not authenticated');
 }
